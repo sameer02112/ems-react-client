44e120aa8bcaf5afdef7dd204241e6c95849f7ec
Sameer Agrawal <sameer02112@gmail.com>
2023-05-19 12:37:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import ListEmployeeComponent from './components/ListEmployeeComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
+import AddEmployeeComponent from './components/AddEmployeeComponent';
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <Router>
+      <HeaderComponent />
+      <div className= "container">
+        <Routes>
+            <Route exact path = "/" element = {<ListEmployeeComponent/>}></Route>
+            <Route path = "/employees" element = {<ListEmployeeComponent/>}></Route>
+            <Route path = "/add-employee" element = {<AddEmployeeComponent/>} ></Route>
+            <Route path = "/edit-employee/:id" element = {<AddEmployeeComponent/>}></Route>
+          </Routes>
+      </div>
+      <FooterComponent />
+      </Router>
+  </div>
   );
 }
 
---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HeaderComponent = () => {
+    return (
+        <div>
+            <header>
+                <nav className = "navbar navbar-expand-md navbar-dark bg-dark">
+                    <div>
+                        <p className = "navbar-brand"> Employee Management Application</p>
+                    </div>
+                </nav>
+            </header>
+        </div>
+    )
+}
+
+export default HeaderComponent
